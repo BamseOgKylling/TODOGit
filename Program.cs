9223438d290f723b89cdd5d781514ca08a195aeb
Ted Sandberg <tedharleysandberg@hotmail.com>
2021-10-01 14:19:46 +0200

    Add initial menu

--- Program.cs
+++ Program.cs
@@ -9,6 +9,52 @@ namespace TODOGit
         static void Main(string[] args)
         {
 
+            bool applicationRunning = true;
+
+            do
+            {
+                CursorVisible = false;
+
+                WriteLine("1. Add task");
+                WriteLine("2. List tasks");
+                WriteLine("3. Exit");
+
+                ConsoleKeyInfo input;
+                bool invalidChoice;
+
+                do
+                {
+                    input = ReadKey(true);
+
+                    invalidChoice = !(input.Key == ConsoleKey.D1 || input.Key == ConsoleKey.NumPad1
+                        || input.Key == ConsoleKey.D2 || input.Key == ConsoleKey.NumPad2
+                        || input.Key == ConsoleKey.D3 || input.Key == ConsoleKey.NumPad3);
+
+                } while (invalidChoice);
+
+                CursorVisible = true;
+
+                Clear();
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.D1:
+
+                        break;
+
+                    case ConsoleKey.D2:
+
+                        break;
+
+                    case ConsoleKey.D3:
+                        applicationRunning = false;
+                        break;
+                }
+
+                Clear();
+
+            } while (applicationRunning);
+                
         }
     }
 }
